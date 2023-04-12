3da451e70ccb16ed34b7228d72005ba9f2f0a0d2
Martin <martin.uglesic@gmail.com>
2023-04-12 11:26:41 +0200

    Updated cards

--- src/routes/cards/+page.svelte
+++ src/routes/cards/+page.svelte
@@ -9,7 +9,7 @@
 
     <div class="container">
 
-        <div class="card">
+        <div id="card-1" class="card">
             <img alt="Card Background" src="https://images.unsplash.com/photo-1599557041284-7e2a15610388?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80">
             <span>
                 <p>Mr. McFly, Mr. McFly, this just arrived, oh hi Marty. I think it's your new book.</p>
@@ -18,16 +18,21 @@
             </span>
         </div>
 
-        <div class="card">
+        <div id="card-2" class="card">
             <img alt="Card Background" src="https://images.unsplash.com/photo-1579033014049-f33d9b14d37e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80">
+            <span>
+                <p>Dammit, Doc, why did you have to tear up that letter? If only I had more time.</p>
+                <p>Wait a minute, I got all the time I want I got a time machine, I'll just go back and warn him. 10 minutes oughta do it.</p>
+                <p>Time-circuits on, flux-capacitor fluxing, engine running, alright. No, no no no no, c'mon c'mon. C'mon c'mon, here we go, this time. Please, please, c'mon.</p>
+            </span>
         </div>
 
-        <div class="card">
-            <img alt="Card Background" src="https://images.unsplash.com/photo-1607622017669-2b0fe5eb878b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1970&q=80">
+        <div id="card-3" class="card">
+            <!-- <img alt="Card Background" src="https://images.unsplash.com/photo-1607622017669-2b0fe5eb878b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1970&q=80"> -->
         </div>
 
-        <div class="card">
-            <img alt="Card Background" src="https://images.unsplash.com/photo-1610247948543-28d438fd094b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80">
+        <div id="card-4" class="card">
+            <!-- <img alt="Card Background" src="https://images.unsplash.com/photo-1610247948543-28d438fd094b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"> -->
         </div>
 
         <!-- <div class="card">
@@ -39,6 +44,12 @@
 </main>
 
 <style>
+
+    :root {
+
+        --gradient-angle: 0deg;
+
+    }
 
     h2 {
 
@@ -73,16 +84,6 @@
 
     }
 
-    /* @container cards (max-width: 400px) {
-
-        .container {
-
-            flex-direction: column;
-
-        }
-
-    } */
-
     .card {
 
         position: relative;
@@ -110,17 +111,21 @@
 
         object-fit: cover;
 
+    }
+
+    #card-1 img {
+
         transition: scale 200ms ease-out, filter 200ms;
 
     }
-    .card:hover img {
+    #card-1:hover img {
 
         scale: 1.05;
         filter: brightness(50%);
 
     }
 
-    .card span {
+    #card-1 span {
 
         display: inline-block;
 
@@ -137,10 +142,122 @@
         opacity: 0;
 
     }
-    .card:hover span {
+    #card-1:hover span {
 
         translate: 0 0;
         opacity: 1;
+
+    }
+
+    #card-2 {
+
+        border-radius: .5em;
+
+        transition: border-radius 400ms;
+
+    }
+    #card-2:hover {
+
+        border-radius: 3em;
+
+    }
+
+    #card-2 img {
+
+        transition: scale 200ms, filter 200ms;
+
+    }
+    #card-2:hover img {
+
+        scale: 1.05;
+        filter: brightness(50%);
+
+    }
+
+    #card-2 span {
+
+        display: inline-block;
+
+        position: absolute;
+
+        top: 50%;
+        transform: translateY(-50%);
+
+        padding: 1em;
+
+        opacity: 0;
+
+        transition: opacity 500ms;
+
+    }
+    #card-2:hover span {
+
+        opacity: 1;
+
+    }
+
+    @property --gradient-angle {
+        syntax: "<angle>";
+        inherits: false;
+        initial-value: 0deg;
+    }
+
+    #card-3 {
+
+        border: unset;
+        overflow: unset;
+
+        isolation: auto;
+
+        background: hsl(209, 45%, 15%);
+
+        border-radius: 1em;
+
+    }
+
+    #card-3::before,
+    #card-3::after {
+
+        content: '';
+
+        position: absolute;
+
+        inset: -.4em;
+
+        z-index: -2;
+
+        background-color: hsl(166, 68%, 53%);
+
+        background-image: conic-gradient(
+            from var(--gradient-angle),
+            hsl(166, 68%, 53%),
+            hsl(213, 100%, 44%),
+            hsl(166, 68%, 53%)
+        );
+
+        border-radius: inherit;
+
+        animation: border_rotate 5s infinite linear;
+
+    }
+
+    #card-3::after {
+        filter: blur(2em);
+    }
+
+    @keyframes border_rotate {
+
+        0% {--gradient-angle: 0deg}
+        100% {--gradient-angle: 360deg}
+
+    }
+
+    #card-4 {
+
+        background: hsl(209, 45%, 15%);
+
+        border-radius: 1em;
+        border: unset;
 
     }
 
