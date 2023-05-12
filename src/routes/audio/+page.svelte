53b233ae31ec3d6dc00cd18e8e6ac1c7c5a68022
Martin <martin.uglesic@gmail.com>
2023-05-12 22:07:16 +0200

    Changed the audio visualizer to SVG

--- src/routes/audio/+page.svelte
+++ src/routes/audio/+page.svelte
@@ -10,8 +10,10 @@
     let dataArray: Uint8Array | undefined;
     let getFrequencyInterval: number;
 
-    let nBar: number = 32;
-    const barWidth: number = 10;
+    const nBarOpts: number[] = [64, 128, 256, 512];
+
+    let nBar: number = 128;
+    const hVis: number = 400;
 
     onMount(() => {
         
@@ -59,21 +61,36 @@
         <div style="display: flex; gap: 5px;">
             <audio id="piece" controls preload="auto" src="{base}/Tchaikovsky.mp3"></audio>
             <select id="barCount" bind:value={nBar}>
-                <option value={16}>16</option>
-                <option value={32} selected>32</option>
-                <option value={64}>64</option>
+                {#each nBarOpts as n}
+                    <option value={n}>{n}</option>
+                {/each}
             </select>
         </div>
 
+        <span style="color: firebrick; font-size: 0.7em;">!!WARNING!!<br/>High values might be slow on some devices</span>
+
         {#if dataArray}
 
-            <div class="visualizer" style="--vis-scale: {48 / nBar};">
+            <div class="visualizer" style="--vis-height: {hVis}px; --vis-scale: {48 / nBar};">
 
-                {#each dataArray as d}
+                <!-- {#each dataArray as d}
                     
-                    <div style="--bar-width: {barWidth}px; --bar-height: {(Math.abs(128 - d) * 2) + barWidth}px;"></div>
+                    <div style="--bar-width: {barWidth}px; --bar-height: {((d / 128.0) * hVis) * 0.5}px;"></div>
 
-                {/each}
+                {/each} -->
+
+                <svg class="visualizer-svg" style="height: 100%;" viewBox="0 0 200 100">
+
+                    {#each dataArray as d, i}
+
+                    {@const w = 200 / nBar}
+
+                        <!-- <line x1={i * w + (w / 2)} y1={100} x2={i * w + (w / 2)} y2={((128.0 / d) * 100 * 0.5)} stroke-width={100 / nBar}></line> -->
+                        <rect x={i * w} y={0} width={w} height={((d / 128.0) * 100 * 0.5)}></rect>
+
+                    {/each}
+
+                </svg>
 
             </div>
 
@@ -83,7 +100,7 @@
 
 </main>
 
-<style>
+<style lang="scss">
 
     main {
         
@@ -129,40 +146,54 @@
     .visualizer {
 
         display: flex;
+        align-items: flex-end;
 
-        height: 200px;
+        gap: 1px;
 
-        transform-origin: top;
+        width: 100%;
+        // height: var(--vis-height);
+
+        transform-origin: center;
         
-        scale: var(--vis-scale);
+        /* scale: var(--vis-scale); */
+        scale: 1 -1;
 
     }
 
-    .visualizer > div {
+    .visualizer-svg rect {
 
-        background: white;
-        border-radius: 100vh;
-
-        width: var(--bar-width);
-        height: min(var(--bar-height), 200px);
+        stroke: var(--text-color);
+        fill: var(--text-color);
 
         transition: height 50ms ease-in-out;
 
     }
 
+    // .visualizer > div {
+
+    //     background: var(--text-color);
+    //     /* border-radius: 100vh 100vh 0 0; */
+
+    //     width: var(--bar-width);
+    //     height: min(var(--bar-height), var(--vis-height));
+
+    //     transition: height 50ms ease-in-out;
+
+    // }
+
     @media (max-width: 500px) {
 
         .wrapper {
 
-            max-width: 100%;
+            max-width: 100vw;
 
         }
 
-        .visualizer {
+        // .visualizer {
 
-            scale: calc(var(--vis-scale) * 0.7);
+        //     scale: calc(var(--vis-scale) * 0.7);
 
-        }
+        // }
 
     }
 
